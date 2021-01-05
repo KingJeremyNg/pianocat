0f7182a6035a4cebb3199eb102dd37434162531c
KingJeremyNg <jeremy97ng@gmail.com>
2021-01-05 18:37:04 -0500

    Update file structure

--- src/index.js
+++ src/index.js
@@ -62,11 +62,11 @@ function handPairs(data) {
         if (data.octaves[0] < 4) left = data.octaves[0];
         display(data, { left, right });
     }
-    else if (data.octaves.length == 2 && data.octaves[0] == data.octaves[1] - 1 && data.keys.length < 5) {
-        if (data.octaves[0] >= 4) right = data.octaves[0];
-        else if (data.octaves[1] < 4) left = data.octaves[0];
-        display(data, { left, right });
-    }
+    // else if (data.octaves.length == 2 && data.octaves[0] == data.octaves[1] - 1 && data.keys.length < 5) {
+    //     if (data.octaves[0] >= 4) right = data.octaves[0];
+    //     else if (data.octaves[1] < 4) left = data.octaves[0];
+    //     display(data, { left, right });
+    // }
     else {
         left = data.octaves[0];
         right = data.octaves[data.octaves.length - 1];
@@ -78,12 +78,12 @@ function display(data, pair) {
     // console.log("left " + pair.left);
     // console.log("right " + pair.right);
     data.text.innerHTML = (`${data.keys}`);
-    data.cat.innerHTML = ("<img src='../src/imgs/catBody.png'>");
+    data.cat.innerHTML = ("<img src='./src/imgs/catBody.png'>");
 
     if (pair.left || pair.right) {
-        if (pair.left) data.leftHand.innerHTML = (`<img src="../src/imgs/catLeftDown${pair.left}.png">`);
+        if (pair.left) data.leftHand.innerHTML = (`<img src="./src/imgs/catLeftDown${pair.left}.png">`);
         else data.leftHand.innerHTML = (`<img src="${json.leftUp}">`);
-        if (pair.right) data.rightHand.innerHTML = (`<img src="../src/imgs/catRightDown${pair.right}.png">`);
+        if (pair.right) data.rightHand.innerHTML = (`<img src="./src/imgs/catRightDown${pair.right}.png">`);
         else data.rightHand.innerHTML = (`<img src="${json.rightUp}">`);
         // console.log(data.leftHand.innerHTML);
         // console.log(data.rightHand.innerHTML);
@@ -98,26 +98,26 @@ function display(data, pair) {
 
 // CURRENTLY USELESS
 var json = {
-    "catDown": "../src/imgs/catDown.png",
-    "catUp": "../src/imgs/catUp.png",
-    "rightUp": "../src/imgs/catRightUp.png",
-    "right8": "../src/imgs/catRight7.png",
-    "right7": "../src/imgs/catRight7.png",
-    "right6": "../src/imgs/catRight6.png",
-    "right5": "../src/imgs/catRight5.png",
-    "right4": "../src/imgs/catRight4.png",
+    "catDown": "./src/imgs/catDown.png",
+    "catUp": "./src/imgs/catUp.png",
+    "rightUp": "./src/imgs/catRightUp.png",
+    "right8": "./src/imgs/catRight7.png",
+    "right7": "./src/imgs/catRight7.png",
+    "right6": "./src/imgs/catRight6.png",
+    "right5": "./src/imgs/catRight5.png",
+    "right4": "./src/imgs/catRight4.png",
     "right3": "",
     "right2": "",
     "right1": "",
     "right0": "",
-    "leftUp": "../src/imgs/catLeftUp.png",
+    "leftUp": "./src/imgs/catLeftUp.png",
     "left8": "",
     "left7": "",
     "left6": "",
     "left5": "",
     "left4": "",
-    "left3": "../src/imgs/catLeft3.png",
-    "left2": "../src/imgs/catLeft2.png",
-    "left1": "../src/imgs/catLeft1.png",
-    "left0": "../src/imgs/catLeft1.png"
+    "left3": "./src/imgs/catLeft3.png",
+    "left2": "./src/imgs/catLeft2.png",
+    "left1": "./src/imgs/catLeft1.png",
+    "left0": "./src/imgs/catLeft1.png"
 }
